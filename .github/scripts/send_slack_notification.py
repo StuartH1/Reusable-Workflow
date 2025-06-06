19cf7171e9147220e9956985bc868d69d8956e11
Stuart Hernandez <stuarth1@brightops.com>
2025-06-06 13:12:10 -0600

    fix: add thread_ts to slack message

--- .github/scripts/send_slack_notification.py
+++ .github/scripts/send_slack_notification.py
@@ -58,6 +58,7 @@ def find_pr_thread(repo_name: str, pr_number: str, bot_id: str = "B06GW8TTWUD"):
         if repo_name not in message.get("text", ""):
             continue
         if "requested your review on" in message.get("text", ""):
+            print(message)
             parent_message = message
             break
     
